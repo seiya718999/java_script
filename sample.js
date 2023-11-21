6c5362a33164cb1c07158bd015f4776975e4d456
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-11-21 00:44:37 +0000

    [finish]java-script-2

--- sample.js
+++ sample.js
@@ -1,3 +1,10 @@
-let str1 = 'Hello';
-let str2 = 'World';
-alert(str1 + str2);
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+  alert('みかんとリンゴが同じ値段');
+} else{
+  alert('みかんの値段がリンゴより高い')
+}
